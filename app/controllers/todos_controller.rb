60a6b6e693e2ce3b7ec6cac072ba6b2de164babf
ViditChitkara <chitkara.vidit@gmail.com>
2017-07-04 14:12:36 +0530

    Fixed a bug

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,7 +6,7 @@ class TodosController < ApplicationController
   # GET /todos
   # GET /todos.json
   def index
-    @todos = Todo.all
+    @todos = current_user.todos
   end
 
   # GET /todos/1
@@ -27,6 +27,7 @@ class TodosController < ApplicationController
   # POST /todos.json
   def create
     @todo = Todo.new(todo_params)
+    @todo.user_id = current_user.id
     respond_to do |format|
       if @todo.save
         format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
